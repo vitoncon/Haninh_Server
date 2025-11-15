c1e7fe687557768da4b8c87bc6e710ac08f374f2
vitoncon <nguyentienvietk9@gmail.com>
2025-11-15 22:38:25 +0700

    add:import trạng thái user

---
+++ migrations/20250113000000_add_status_to_users_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.table('users', function(table) {
+        table.string('status').notNullable().defaultTo('Hoạt động').comment('User status: Hoạt động, Không hoạt động, Tạm khóa');
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('users', function(table) {
+        table.dropColumn('status');
+    });
+};
